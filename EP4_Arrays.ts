d6abed0631da854113e2cfe87a4c04fe0e383dab
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-04-12 09:05:56 +0530

    union

--- EP4_Arrays.ts
+++ EP4_Arrays.ts
@@ -29,3 +29,73 @@ allUsers.push({
     name: "", // Attempting to assign an empty string to name
     isActive: true,
 });
+
+// $ Union Type
+// -> Use union type instead of any in TS (Recommended)
+// -> In TypeScript, a union type allows you to declare a variable that can hold values of different types. You define a union type by using the pipe | symbol between the types you want to include
+
+// @EG 1
+// Defining a union type
+let myVar: number | string;
+
+// Assigning values
+myVar = 10; // Valid
+myVar = "Hello"; // Valid
+// myVar = true; // ? Error: Type 'boolean' is not assignable to type 'number | string'
+
+//  @ EG 2
+type User = {
+    name : string;
+    id:number;
+}
+
+type Admin = {
+    username:string;
+    id:number
+}
+
+let aman: User | Admin = {name: "Aman", id:12}
+
+// Allowed to redefined users
+aman = {username: "Aman", id:12}
+
+//  @ EG 3
+
+
+getDBId(3)
+getDBId("3");
+
+// function getDBId(id: number | string){
+//     console.log(`DB id is : ${id}`);
+//     id.toLowerCase();
+// }
+// ?  It will throw error as toLowerCase is not applicable on numbers 
+// -> So for that we will do a check if id is a string then only toLowerCase will be applied
+
+function getDBId(id: number | string){
+    if(typeof id === 'string'){
+    id.toLowerCase();
+    }
+}
+
+/* const data: number[] = [1,2,3];
+const data2: string[] = ['1','2','3'];
+// const data3: any[] = ['1','2',3, true];
+// -> use union instead of any 
+ const data3: (string | number | boolean) [] = ['1','2',3, true];
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
